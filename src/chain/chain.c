9392fbd0ca72764450b3a27ab7c8d2b8426e0d69
Eug-VS <eug-vs@keemail.me>
2019-12-29 17:33:18 +0300

    Fix indentation and expand for loops

--- src/chain/chain.c
+++ src/chain/chain.c
@@ -9,7 +9,7 @@ Chain new_chain(size_t width) {
 }
 
 bool is_empty(Chain chain) {
-	return *chain.head == NULL;
+    return *chain.head == NULL;
 }
 
 void* advance(Chain chain, void* node) {
@@ -18,82 +18,86 @@ void* advance(Chain chain, void* node) {
 
 int length(Chain chain) {
     int length = 0;
-    void** node = chain.head;
-    while(*node) {
-        node = *node;
-        node = advance(chain, node);
+    for (
+        void** node = chain.head;
+        (node = *node);
+        node = advance(chain, node)
+    ){
         length++;
     }
     return length;
 }
 
 void* link(Chain chain) {
-	int node_size = chain.width + PTR_SIZE;
-	void* new_node = malloc(node_size);
-	memset(new_node, 0, node_size);
+    int node_size = chain.width + PTR_SIZE;
+    void* new_node = malloc(node_size);
+    memset(new_node, 0, node_size);
 
-	if (is_empty(chain)) {
-		*chain.head = new_node;
-	} else {
-		void** node = chain.head;
-    	while (*node) {
-        	node = *node;
-        	node = advance(chain, node);
-		}
-    	*node = new_node;
-	}
+    if (is_empty(chain)) {
+        *chain.head = new_node;
+    } else {
+        void** node = chain.head;
+        while (*node) {
+            node = *node;
+            node = advance(chain, node);
+        }
+        *node = new_node;
+    }
 
     return new_node;
 }
 
 void* pull(Chain chain, int index) {
-	if (!is_empty(chain)) {
-		void** node = chain.head;
-		int i = 0;
-    	while (*node) {
-        	node = *node;
-			if (i == index) return node;
-        	node = advance(chain, node);
-			i++;
-		}
-	}
+    if (!is_empty(chain)) {
+        int i = 0;
+        for (
+            void** node = chain.head;
+            (node = *node);
+            node = advance(chain, node)
+        ){
+            if (i == index) return node;
+            i++;
+        }
+    }
     return NULL;
 }
 
 void print(Chain chain) {
-	if (is_empty(chain)) {
-		printf("Chain nodes: ");
-	} else {
-		void** node = chain.head;
-		while (*node) {
-			node = *node;
-			printf("%d ", *(int*)node);
-        	node = advance(chain, node);
-		}
-		printf("\n");
-	}
+    if (is_empty(chain)) {
+        printf("Chain nodes: ");
+    } else {
+        for (
+            void** node = chain.head;
+            (node = *node);
+            node = advance(chain, node)
+        ){
+            printf("%d ", *(int*)node);
+        }
+        printf("\n");
+    }
 }
 
 void release(Chain chain) {
-	if (!is_empty(chain)) {
-		void** node = chain.head;
-		void* killer = NULL;
-		while (*node) {
-			node = *node;
-			if (killer) {
-				free(killer);
-			}
-			killer = node;
-        	node = advance(chain, node);
-		}
-		free(killer);
-		*chain.head = NULL;
-	}
+    if (!is_empty(chain)) {
+        void* killer = NULL;
+        for (
+            void** node = chain.head;
+            (node = *node);
+            node = advance(chain, node)
+        ){
+            if (killer) {
+                free(killer);
+            }
+            killer = node;
+        }
+        free(killer);
+        *chain.head = NULL;
+    }
 }
 
 
 void disassemble(Chain chain) {
-	release(chain);
-	free(chain.head);
+    release(chain);
+    free(chain.head);
 }
 
